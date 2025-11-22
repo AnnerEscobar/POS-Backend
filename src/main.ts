039c0a85aefd59beb2e8ef5371a1df9f42c4d5b7
Anner Escobar <anner123escobar@gmail.com>
2025-11-21 20:57:01 -0600

    Enable CORS for Netlify frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:4200'], credentials: true });
+  app.enableCors({ origin: ['http://localhost:4200', 'https://pos-dem0.netlify.app'], credentials: true });
   /* app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true })); */
   await app.listen(process.env.PORT ?? 3000);
   /* await app.listen(3000, '0.0.0.0'); */
